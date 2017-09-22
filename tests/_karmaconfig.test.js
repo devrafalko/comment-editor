9b391305cbdc9d718ef30d05f76a2830e7493674
mph <mph@mph.info>
2017-09-22 15:34:24 -0700

    Add test for issue #1

---
+++ tests/_karmaconfig.test.js
@@ -0,0 +1,22 @@
+import 'karma-html'
+import { customMatchers } from 'jasmine-dom-custom-matchers'
+
+describe('karma setup', function() {
+  beforeAll(function() {
+    jasmine.addMatchers(customMatchers)
+
+    karmaHTML.index.onstatechange = function(ready) {
+      if (ready) done() //if the #Document is ready, fire tests!
+    }
+    karmaHTML.index.open()
+  })
+
+  it('should load a real Document object', function() {
+    let iframeDocument = karmaHTML.index.document
+    console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+    console.log('karmaHTML.index:', karmaHTML.index.document)
+    console.log('document_0:', window.document_0)
+    console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+    expect(iframeDocument.constructor.name).toEqual('HTMLDocument')
+  })
+})
